48fc2061e472150f4ef57330bbd188e374a3c27e
Jorgemayor <jorge.e.mayor@gmail.com>
2023-11-04 17:20:45 +0300

    feat: add table sql scripts

---
+++ drop.sql
@@ -0,0 +1,25 @@
+DROP TABLE trainers;
+
+DROP TABLE teams;
+
+DROP TABLE tournaments;
+
+DROP TABLE trainers_tournaments;
+
+DROP TABLE regulations;
+
+DROP TABLE matches;
+
+DROP TABLE pokemon;
+
+DROP TABLE movements;
+
+DROP TABLE pokemon_movements;
+
+DROP TABLE abilities;
+
+DROP TABLE natures;
+
+DROP TABLE objects;
+
+DROP TABLE selected_pokemon; 
