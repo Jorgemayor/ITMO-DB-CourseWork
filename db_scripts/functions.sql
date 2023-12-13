063d022e660b14e227d715d0df307efe9faec3f5
Jorgemayor <jorge.e.mayor@gmail.com>
2023-12-13 06:01:38 -0500

    feat: add check for teams and top OU

--- db_scripts/functions.sql
+++ db_scripts/functions.sql
@@ -1,8 +1,16 @@
---CREATE OR REPLACE FUNCTION verify_pkmn() RETURN BOOLEAN
---BEGIN
-
-
--- END;
+CREATE OR REPLACE FUNCTION GetTopOU() RETURNS TABLE(p_name TEXT, use BIGINT) AS
+$$
+BEGIN
+	RETURN QUERY
+	SELECT * FROM (
+		SELECT name AS p_name, COUNT(*) AS use
+		FROM selected_pokemon JOIN pokemon ON id_pokemon = pokemon.id
+		GROUP BY name
+	) AS sub
+	ORDER BY use DESC
+	LIMIT 5;
+END
+$$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION VerifyTournamentFormat() RETURNS TRIGGER AS
 $$
@@ -10,17 +18,30 @@ DECLARE
 	format INTEGER;
 BEGIN
 	SELECT id_format into format FROM tournaments WHERE id = NEW.id_tournament;
-	if (SELECT COUNT(*) 
+	IF (SELECT COUNT(*) 
 		FROM teams
 		WHERE id_trainer = NEW.id_trainer
 		AND id_format = format) <= 0
-	then
+	THEN
 		DELETE FROM trainers_tournaments
 		WHERE id_trainer = NEW.id_trainer
 		AND id_tournament = NEW.id_tournament;
 		
 		RAISE EXCEPTION 'Trainer % has no teams for this format', NEW.id_trainer;
-	end if;
+	END IF;
+	RETURN NEW;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION VerifyTeamMembers() RETURNS TRIGGER AS
+$$
+BEGIN
+	IF (SELECT COUNT(*)
+		FROM selected_pokemon
+		WHERE id_trainer = NEW.id_trainer) >= 6
+	THEN
+		RAISE EXCEPTION 'Team % already has 6 members', NEW.id_team;
+	END IF;
 	RETURN NEW;
 END
 $$ LANGUAGE plpgsql;
--- db_scripts/triggers.sql
+++ db_scripts/triggers.sql
@@ -1,7 +1,15 @@
-DROP TRIGGER IF EXISTS "VerifyTeamAvailable" ON "trainers_torunaments";
+DROP TRIGGER IF EXISTS "verifyteamavailable" ON trainers_tournaments;
 CREATE TRIGGER VerifyTeamAvailable
 	AFTER INSERT
-	ON "trainers_tournaments"
+	ON trainers_tournaments
 	FOR EACH ROW
 EXECUTE FUNCTION "verifytournamentformat"();
+
+
+DROP TRIGGER IF EXISTS "verifyteammembers" ON selected_pokemon;
+CREATE TRIGGER VerifyTeamMembers
+	BEFORE INSERT
+	ON selected_pokemon
+	FOR EACH ROW
+EXECUTE FUNCTION "verifyteammembers"();
 
