d36c9ab507eaa7b130920a4c1457d6e7421dadb5
jorgemayor <jorge.e.mayor@gmail.com>
2024-04-02 20:47:10 +0300

    feat: add card images

--- teambuilder/src/components/Cards.js
+++ teambuilder/src/components/Cards.js
@@ -10,13 +10,13 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-9.jpg'
+              src='images/competitivePkmn.jpg'
               text='Create your own competitive Pokémon team'
               label='Teams'
               path='/teams'
             />
             <CardItem
-              src='images/img-2.jpg'
+              src='images/pcBox.jpg'
               text='Manage your teams'
               label='Teams'
               path='/teams'
@@ -24,19 +24,19 @@ function Cards() {
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-3.jpg'
+              src='images/tournaments.jpg'
               text='Register for tournaments in the platform'
               label='Tournaments'
               path='/tournaments'
             />
             <CardItem
-              src='images/img-4.jpg'
+              src='images/compTeam.png'
               text="Check other player's teams"
               label='Teams'
               path='/teams'
             />
             <CardItem
-              src='images/img-8.jpg'
+              src='images/moreComing.jpg'
               text='And much more comming!'
               label='Home'
               path='/'
