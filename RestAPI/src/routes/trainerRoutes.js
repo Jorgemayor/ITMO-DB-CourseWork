0d7d09fc1bd9e4c5a8ef6ec8fa88e86bab527d4f
jorgemayor <jorge.e.mayor@gmail.com>
2024-04-03 06:28:38 +0300

    feat: add utils

--- RestAPI/src/routes/trainerRoutes.js
+++ RestAPI/src/routes/trainerRoutes.js
@@ -6,7 +6,7 @@ const trainerAuth = require('../middleware/trainerAuth')
 const router = Router()
 
 router.post('/signup', trainerAuth.saveTrainer, signup)
-router.post('/login', login )
+router.post('/login', login)
 router.get('/logout', loggingOut)
 
 module.exports = router
--- teambuilder/src/components/pages/Teams.js
+++ teambuilder/src/components/pages/Teams.js
@@ -1,73 +1,70 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, {useEffect, useState} from 'react'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye, faPencil } from '@fortawesome/free-solid-svg-icons'
 import '../../App.css'
 import './Teams.css'
+import {get} from "../../utils/fetcher";
 
 function Teams() {
+    const [teamList, setTeamList] = useState([])
+    const [showTrainerTeams, setShowTrainerTeams] = useState(true)
+    const trainerId = 1
 
-  const [teamList, setTeamList] = useState([])
-  const [showTrainerTeams, setShowTrainerTeams] = useState(true)
-  const trainerId = 1
-
-  useEffect(() => {
-    const fetchTeams = async () => {
-      try {
-        let url = ''
-        if (showTrainerTeams) {
-          url = `http://localhost:3001/api/team/trainer/${trainerId}`
-        } else {
-          url = 'http://localhost:3001/api/team'
+    useEffect(() => {
+        const fetchTeams = async () => {
+            const url = showTrainerTeams ? `/api/team/trainer/${trainerId}` : '/api/team';
+            try {
+                const teamResponse = await get(url);
+                setTeamList(teamResponse);
+            } catch (error) {
+                console.error('Error fetching data:', error)
+            }
         }
-        const teamResponse = await axios.get(url)
-        setTeamList(teamResponse.data)
-      } catch (error) {
-        console.error('Error fetching data:', error)
-      }
+
+        fetchTeams()
+    }, [showTrainerTeams])
+
+    const changeTeams = () => {
+        setShowTrainerTeams(!showTrainerTeams)
     }
 
-    fetchTeams()
-  }, [showTrainerTeams])
-
-  const changeTeams = () => {
-    setShowTrainerTeams(!showTrainerTeams)
-  }
-
-  return (
-    <>
-      <button onClick={changeTeams}>
-        {showTrainerTeams ? 'Show public teams' : 'Show trainer teams'}
-      </button>
-      <div className="table-container">
-        <h2>Teams</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Format</th>
-              {showTrainerTeams && <th>Privacy</th>}
-              <th>Generation</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {teamList && teamList.map(team => (
-              <tr key={team.id}>
-                <td>{team.name}</td>
-                <td>{team.format.name}</td>
-                {showTrainerTeams && <td>{team.private ? "Yes" : "No"}</td>}
-                <td>{team.format.generation}</td>
-                <td>
-                  <button>
-                    {showTrainerTeams ? 'Edit' : 'See'}
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <button onClick={changeTeams}>
+                {showTrainerTeams ? 'Show public teams' : 'Show trainer teams'}
+            </button>
+            <div className="table-container">
+                <h2>Teams</h2>
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Format</th>
+                        {showTrainerTeams && <th>Privacy</th>}
+                        <th>Generation</th>
+                        <th>Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {teamList && teamList.map(team => (
+                        <tr key={team.id}>
+                            <td>{team.name}</td>
+                            <td>{team.format.name}</td>
+                            {showTrainerTeams && <td>{team.private ? "Yes" : "No"}</td>}
+                            <td>{team.format.generation}</td>
+                            <td>
+                                <Link to='/' className='navbar-logo'>
+                                    <FontAwesomeIcon icon={showTrainerTeams ? faPencil : faEye} style={{color: "#000000"}}/>
+                                </Link>
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
+    )
 }
 
 export default Teams
