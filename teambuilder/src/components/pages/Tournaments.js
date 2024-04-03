1ae076b1f5ea377a2f86c7f1dc5656e9db31dcd6
jorgemayor <jorge.e.mayor@gmail.com>
2024-04-03 04:12:12 +0300

    feat: featch tournaments

--- teambuilder/src/components/pages/Tournaments.js
+++ teambuilder/src/components/pages/Tournaments.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import '../../App.css'
 
 function Tournaments() {
+  const [tournamentList, setTournamentList] = useState([])
+
+  useEffect(() => {
+    const fetchTournaments = async () => {
+      try {
+        const tournamentResponse = await axios.get('http://localhost:3001/api/tournament')
+        setTournamentList(tournamentResponse.data)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    fetchTournaments()
+  }, [])
+
+  useEffect(() => {
+    console.log(tournamentList)
+  }, [tournamentList])
+
   return (
     <>
-      <h1 className='tournaments'>Tournaments</h1>
+      
     </>
   )
 }
